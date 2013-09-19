124e21048402a54457463d37e3248865c93857c1
Gustavo Brunoro <brunoro@devio.us>
2013-09-19 00:42:35 -0300

    ports and references also should be escaped
    * yay

--- lib/iex/lib/iex/debugger/escape.ex
+++ lib/iex/lib/iex/debugger/escape.ex
@@ -2,16 +2,23 @@ defmodule IEx.Debugger.Escape do
 
   @pid_prefix "__PID_"
   @fun_prefix "__FUN_"
+  @ref_prefix "__REF_"
+  @port_prefix "__PORT_"
 
   defmacro is_escapable(thing) do
     quote do
-      is_pid(unquote(thing)) or is_function(unquote(thing))
+      is_pid(unquote(thing)) 
+      or is_function(unquote(thing))
+      or is_reference(unquote(thing))
+      or is_port(unquote(thing))
     end
   end
 
   def is_escaped?(bin) do
     String.starts_with?(bin, @pid_prefix) or
-    String.starts_with?(bin, @fun_prefix)
+    String.starts_with?(bin, @fun_prefix) or
+    String.starts_with?(bin, @ref_prefix) or
+    String.starts_with?(bin, @port_prefix) 
   end
 
   # #PID<0.49.0> -> :__PID_0_49_0__
@@ -35,5 +42,27 @@ defmodule IEx.Debugger.Escape do
 
 
     "#{@fun_prefix}#{fun_name}__"
+  end
+
+  # #Reference<0.0.0.76> -> :__REF_0_0_0_76__
+  def escape(ref) when is_reference(ref) do
+    esc_ref = ref |> :erlang.ref_to_list
+                  |> to_string
+                  |> String.replace("#Ref<", "")
+                  |> String.replace(">", "")
+                  |> String.replace(".", "_") 
+
+    "#{@ref_prefix}#{esc_ref}__"
+  end
+
+  # #Port<0.1287> -> :__PORT_0_1287__
+  def escape(port) when is_port(port) do
+    esc_ref = port |> :erlang.port_to_list
+                   |> to_string
+                   |> String.replace("#Port<", "")
+                   |> String.replace(">", "")
+                   |> String.replace(".", "_") 
+
+    "#{@port_prefix}#{esc_ref}__"
   end
 end
